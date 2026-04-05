0af3ee2eaba498da2480fddd15ead3f27248240a
RosenStefanov <rkstefanov@abv.bg>
2026-04-05 23:42:58 +0300

    feat: [NWC-0003] add project dependencies

--- core/testing/build.gradle.kts
+++ core/testing/build.gradle.kts
@@ -10,4 +10,7 @@ dependencies {
     implementation(project(":core:common"))
     implementation(libs.junit)
     implementation(libs.kotlinx.coroutines.test)
+    implementation(libs.mockk)
+    implementation(libs.turbine)
+    implementation(libs.truth)
 }
